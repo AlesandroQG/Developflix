9a197ca3df11a22978ba12636dd8f857e2cb62f0
Alesandro Quirós Gobbato <alesandroquirosg@icloud.com>
2025-11-11 11:23:17 +0100

    ejercicio corregido en clase

--- js/script.js
+++ js/script.js
@@ -1,40 +1,20 @@
-import peliculas from './peliculas.js'
+import peliculas from './peliculas.js';
 
-const base_url = "https://image.tmdb.org/t/p/w500";
+console.log(peliculas);
 
-const lista_accion = document.getElementById("genero-28");
-const lista_thriller = document.getElementById("genero-53");
-const lista_aventura = document.getElementById("genero-12");
+function filtrarPeliculas(genre_id) {
+    const img_base_url = "https://image.tmdb.org/t/p/w500";
+    const template = (pelicula) => {
+        return `<div class="pelicula">
+            <img src="${img_base_url + pelicula.poster_path}" alt="${pelicula.title}">
+            <h3>${pelicula.title}</h3>
+        </div>`;
+    };
+    const filtradas = peliculas.filter((pelicula) => pelicula.genre_ids.includes(genre_id));
+    const lista = document.getElementById(`genero-${genre_id}`);
+    lista.innerHTML = filtradas.map((pelicula) => template(pelicula)).join("");
+}
 
-const accion = peliculas.filter((pelicula) => {
-    return pelicula.genre_ids.includes(28)
-});
-accion.forEach((pelicula) => {
-    const contenedor = `<div class="pelicula">
-    <img src="${base_url + pelicula.poster_path}">
-    <h2>${pelicula.title}</h2>
-    </div>`;
-    lista_accion.innerHTML += contenedor;
-});
-
-const thriller = peliculas.filter((pelicula) => {
-    return pelicula.genre_ids.includes(53)
-});
-thriller.forEach((pelicula) => {
-    const contenedor = `<div class="pelicula">
-    <img src="${base_url + pelicula.poster_path}">
-    <h2>${pelicula.title}</h2>
-    </div>`;
-    lista_thriller.innerHTML += contenedor;
-});
-
-const aventura = peliculas.filter((pelicula) => {
-    return pelicula.genre_ids.includes(12)
-});
-aventura.forEach((pelicula) => {
-    const contenedor = `<div class="pelicula">
-    <img src="${base_url + pelicula.poster_path}">
-    <h2>${pelicula.title}</h2>
-    </div>`;
-    lista_aventura.innerHTML += contenedor;
-});
+filtrarPeliculas(28); // Acción
+filtrarPeliculas(53); // Thriller
+filtrarPeliculas(12); // Aventura
